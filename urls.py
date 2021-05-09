477bb10b68b169125217f85b6b2379962b49043d
Malijai <malijai.caulet.ippm@ssss.gouv.qc.ca>
2021-05-09 10:10:21 -0400

    Changement des styles et pagination alphabétique

--- urls.py
+++ urls.py
@@ -1,16 +1,13 @@
-from django.conf.urls import url
-
+from django.urls import path
 from . import views
-from .views import listing, parcategorie, slideshow
-
+from .views import parcategorie, slideshow
 
 
 urlpatterns = [
-    url(r'^$', listing, name='rcindex'),
-    url(r'^illustration/(?P<oeuvre>[-\w]+)/$', slideshow, name='rcslide'),
-    url(r'^livrets/(?P<catego>[-\w]+)/$', parcategorie, name='rcindexlivrets'),
-    url(r'^categorie/(?P<catego>[-\w]+)/$', parcategorie , name='rcindexcatego'),
-    #     url(r'^(?P<pid>[-\w]+)/dossier/new/$', views.dossier_new, name='dossier_new'),
+    path('', views.listing, name='rcindex'),
+    path('illustration/<int:oeuvre>/', slideshow, name='rcslide'),
+    path('livrets/<int:catego>/', parcategorie, name='rcindexlivrets'),
+    path('categorie/<int:catego>/', parcategorie , name='rcindexcatego'),
 ]
 
 
--- views.py
+++ views.py
@@ -1,7 +1,6 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from RCaulet.models import Oeuvre, Categorie
-
+from .models import Oeuvre, Categorie
 
 
 def listing(request):
